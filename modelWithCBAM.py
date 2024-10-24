a6ea23813c64ccf97b5a81bb751af996b10edc8d
Z <853164830@qq.com>
2024-10-24 18:02:07 +0800

    加入CBAM

--- modelWithCBAM.py
+++ modelWithCBAM.py
@@ -22,6 +22,7 @@ def sava_data(filename, data):
     pickle.dump(data, f)
     f.close()
 
+
 def load_data(filename):
     print("Begin to load data：", filename)
     f = open(filename, 'rb')
@@ -29,28 +30,33 @@ def load_data(filename):
     f.close()
     return data
 
-def get_accuracy(labels, prediction):    
+
+def get_accuracy(labels, prediction):
     cm = confusion_matrix(labels, prediction)
-    #sns.heatmap(cm, annot=True, fmt='d', cmap='Blues')
-    def linear_assignment(cost_matrix):    
+
+    # sns.heatmap(cm, annot=True, fmt='d', cmap='Blues')
+    def linear_assignment(cost_matrix):
         _, x, y = lap.lapjv(cost_matrix, extend_cost=True)
         return np.array([[y[i], i] for i in x if i >= 0])
+
     def _make_cost_m(cm):
         s = np.max(cm)
         return (- cm + s)
+
     indexes = linear_assignment(_make_cost_m(cm))
     js = [e[1] for e in sorted(indexes, key=lambda x: x[0])]
-    cm2 = cm[:, js]    
+    cm2 = cm[:, js]
     accuracy = np.trace(cm2) / np.sum(cm2)
-    return accuracy 
+    return accuracy
+
 
 def get_MCM_score(labels, predictions):
     accuracy = get_accuracy(labels, predictions)
-    precision, recall, f_score, true_sum, MCM = precision_recall_fscore_support(labels, predictions,average='macro')
+    precision, recall, f_score, true_sum, MCM = precision_recall_fscore_support(labels, predictions, average='macro')
     tn = MCM[:, 0, 0]
     fp = MCM[:, 0, 1]
-    fn = MCM[:, 1, 0] 
-    tp = MCM[:, 1, 1] 
+    fn = MCM[:, 1, 0]
+    tp = MCM[:, 1, 1]
     fpr_array = fp / (fp + tn)
     fnr_array = fn / (tp + fn)
     f1_array = 2 * tp / (2 * tp + fp + fn)
@@ -58,19 +64,20 @@ def get_MCM_score(labels, predictions):
     M_fpr = fpr_array.mean()
     M_fnr = fnr_array.mean()
     M_f1 = f1_array.mean()
-    W_fpr = (fpr_array * sum_array).sum() / sum( sum_array )
-    W_fnr = (fnr_array * sum_array).sum() / sum( sum_array )
-    W_f1 = (f1_array * sum_array).sum() / sum( sum_array )
+    W_fpr = (fpr_array * sum_array).sum() / sum(sum_array)
+    W_fnr = (fnr_array * sum_array).sum() / sum(sum_array)
+    W_f1 = (f1_array * sum_array).sum() / sum(sum_array)
     return {
         "M_fpr": format(M_fpr * 100, '.3f'),
         "M_fnr": format(M_fnr * 100, '.3f'),
-        "M_f1" : format(M_f1 * 100, '.3f'),
+        "M_f1": format(M_f1 * 100, '.3f'),
         "W_fpr": format(W_fpr * 100, '.3f'),
         "W_fnr": format(W_fnr * 100, '.3f'),
-        "W_f1" : format(W_f1 * 100, '.3f'),
-        "ACC"  : format(accuracy * 100, '.3f'),
-        "MCM" : MCM
+        "W_f1": format(W_f1 * 100, '.3f'),
+        "ACC": format(accuracy * 100, '.3f'),
+        "MCM": MCM
     }
+
 
 class TraditionalDataset(Dataset):
     def __init__(self, texts, targets, max_len, hidden_size):
@@ -85,7 +92,7 @@ class TraditionalDataset(Dataset):
     def __getitem__(self, idx):
         feature = self.texts[idx]
         target = self.targets[idx]
-        vectors = numpy.zeros(shape=(3,self.max_len,self.hidden_size))
+        vectors = numpy.zeros(shape=(3, self.max_len, self.hidden_size))
         for j in range(3):
             for i in range(min(len(feature[0]), self.max_len)):
                 vectors[j][i] = feature[j][i]
@@ -94,11 +101,12 @@ class TraditionalDataset(Dataset):
             'targets': torch.tensor(target, dtype=torch.long)
         }
 
+
 class TextCNN(nn.Module):
     def __init__(self, hidden_size):
         super(TextCNN, self).__init__()
-        self.filter_sizes = (1, 2, 3, 4, 5, 6, 7, 8, 9, 10)           
-        self.num_filters = 32                                        
+        self.filter_sizes = (1, 2, 3, 4, 5, 6, 7, 8, 9, 10)
+        self.num_filters = 32
         classifier_dropout = 0.1
         self.convs = nn.ModuleList(
             [nn.Conv2d(3, self.num_filters, (k, hidden_size)) for k in self.filter_sizes])
@@ -120,12 +128,89 @@ class TextCNN(nn.Module):
         return out, hidden_state
 
 
+# 通道注意力模块
+class ChannelAttention(nn.Module):
+    def __init__(self, in_channels, ratio=16):
+        super(ChannelAttention, self).__init__()
+        self.avg_pool = nn.AdaptiveAvgPool2d(1)
+        self.max_pool = nn.AdaptiveMaxPool2d(1)
+
+        self.fc1 = nn.Conv2d(in_channels, in_channels // ratio, 1, bias=False)
+        self.relu1 = nn.ReLU()
+        self.fc2 = nn.Conv2d(in_channels // ratio, in_channels, 1, bias=False)
+
+        self.sigmoid = nn.Sigmoid()
+
+    def forward(self, x):
+        avg_out = self.fc2(self.relu1(self.fc1(self.avg_pool(x))))
+        max_out = self.fc2(self.relu1(self.fc1(self.max_pool(x))))
+        out = avg_out + max_out
+        return self.sigmoid(out)
+
+
+# 空间注意力模块
+class SpatialAttention(nn.Module):
+    def __init__(self, kernel_size=7):
+        super(SpatialAttention, self).__init__()
+        assert kernel_size in (3, 7), 'kernel size must be 3 or 7'
+        padding = 3 if kernel_size == 7 else 1
+
+        self.conv1 = nn.Conv2d(2, 1, kernel_size, padding=padding, bias=False)
+        self.sigmoid = nn.Sigmoid()
+
+    def forward(self, x):
+        avg_out = torch.mean(x, dim=1, keepdim=True)
+        max_out, _ = torch.max(x, dim=1, keepdim=True)
+        x = torch.cat([avg_out, max_out], dim=1)
+        x = self.conv1(x)
+        return self.sigmoid(x)
+
+
+# TextCNN 模型中集成 CBAM
+class TextCNNWithCBAM(nn.Module):
+    def __init__(self, hidden_size):
+        super(TextCNNWithCBAM, self).__init__()
+        self.filter_sizes = (1, 2, 3, 4, 5, 6, 7, 8, 9, 10)
+        self.num_filters = 32
+        classifier_dropout = 0.1
+        self.convs = nn.ModuleList(
+            [nn.Conv2d(3, self.num_filters, (k, hidden_size)) for k in self.filter_sizes])
+        self.channel_attentions = nn.ModuleList(
+            [ChannelAttention(self.num_filters) for _ in self.filter_sizes])
+        self.spatial_attentions = nn.ModuleList(
+            [SpatialAttention() for _ in self.filter_sizes])
+        self.dropout = nn.Dropout(classifier_dropout)
+        num_classes = 2
+        self.fc = nn.Linear(self.num_filters * len(self.filter_sizes), num_classes)
+
+    def conv_and_pool(self, x, conv, channel_attention, spatial_attention):
+        x = F.relu(conv(x))
+        x = channel_attention(x) * x
+        x = spatial_attention(x) * x
+        x = x.squeeze(3)
+        x = F.max_pool1d(x, x.size(2)).squeeze(2)
+        return x
+
+    def forward(self, x):
+        out = x.float()
+        # out = out.unsqueeze(1)
+        hidden_states = []
+        for i, conv in enumerate(self.convs):
+            channel_attention = self.channel_attentions[i]
+            spatial_attention = self.spatial_attentions[i]
+            hidden_state = self.conv_and_pool(out, conv, channel_attention, spatial_attention)
+            hidden_states.append(hidden_state)
+        hidden_state = torch.cat(hidden_states, 1)
+        out = self.dropout(hidden_state)
+        out = self.fc(out)
+        return out, hidden_state
 
 
 class CNN_Classifier():
-    def __init__(self, max_len=100, n_classes=2, epochs=100, batch_size = 32, learning_rate = 0.001, \
-                    result_save_path = "/root/data/qm_data/vulcnn/data/results", item_num = 0, hidden_size = 128):
-        self.model = TextCNN(hidden_size)
+    def __init__(self, max_len=100, n_classes=2, epochs=100, batch_size=32, learning_rate=0.001, \
+                 result_save_path="/root/data/qm_data/vulcnn/data/results", item_num=0, hidden_size=128):
+        # self.model = TextCNN(hidden_size)
+        self.model = TextCNNWithCBAM(hidden_size)
         self.device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
         self.max_len = max_len
         self.epochs = epochs
@@ -133,11 +218,11 @@ class CNN_Classifier():
         self.learning_rate = learning_rate
         self.model.to(self.device)
         self.hidden_size = hidden_size
-        result_save_path = result_save_path + "/" if result_save_path[-1]!="/" else result_save_path
+        result_save_path = result_save_path + "/" if result_save_path[-1] != "/" else result_save_path
         if not os.path.exists(result_save_path): os.makedirs(result_save_path)
         self.result_save_path = result_save_path + str(item_num) + "_epo" + str(epochs) + "_bat" + str(batch_size) + ".result"
 
-    def preparation(self, X_train,  y_train, X_valid, y_valid):
+    def preparation(self, X_train, y_train, X_valid, y_valid):
         # create datasets
         self.train_set = TraditionalDataset(X_train, y_train, self.max_len, self.hidden_size)
         self.valid_set = TraditionalDataset(X_valid, y_valid, self.max_len, self.hidden_size)
@@ -167,22 +252,22 @@ class CNN_Classifier():
             vectors = data["vector"].to(self.device)
             targets = data["targets"].to(self.device)
             with autocast():
-                outputs,_  = self.model( vectors )
+                outputs, _ = self.model(vectors)
                 loss = self.loss_fn(outputs, targets)
             scaler.scale(loss).backward()
             scaler.step(self.optimizer)
             scaler.update()
-            preds = torch.argmax(outputs, dim=1).flatten()           
-            
+            preds = torch.argmax(outputs, dim=1).flatten()
+
             losses.append(loss.item())
-            predictions += list(np.array(preds.cpu()))   
-            labels += list(np.array(targets.cpu()))      
+            predictions += list(np.array(preds.cpu()))
+            labels += list(np.array(targets.cpu()))
 
             torch.nn.utils.clip_grad_norm_(self.model.parameters(), max_norm=1.0)
 
             self.scheduler.step()
             progress_bar.set_description(
-                f'loss: {loss.item():.3f}, acc : {(torch.sum(preds == targets)/len(targets)):.3f}')
+                f'loss: {loss.item():.3f}, acc : {(torch.sum(preds == targets) / len(targets)):.3f}')
         train_loss = np.mean(losses)
         score_dict = get_MCM_score(labels, predictions)
         return train_loss, score_dict
@@ -207,17 +292,16 @@ class CNN_Classifier():
 
                 pre += list(np.array(preds.cpu()))
                 label += list(np.array(targets.cpu()))
-                
+
                 losses.append(loss.item())
                 progress_bar.set_description(
-                f'loss: {loss.item():.3f}, acc : {(torch.sum(preds == targets)/len(targets)):.3f}')
+                    f'loss: {loss.item():.3f}, acc : {(torch.sum(preds == targets) / len(targets)):.3f}')
         val_acc = correct_predictions.double() / len(self.valid_set)
-        print("val_acc : ",val_acc)
+        print("val_acc : ", val_acc)
         score_dict = get_MCM_score(label, pre)
         val_loss = np.mean(losses)
         return val_loss, score_dict
 
-    
     def train(self):
         learning_record_dict = {}
         train_table = PrettyTable(['typ', 'epo', 'loss', 'M_fpr', 'M_fnr', 'M_f1', 'W_fpr', 'W_fnr', 'W_f1', 'ACC'])
@@ -233,6 +317,6 @@ class CNN_Classifier():
             print(test_table)
             print("\n")
             learning_record_dict[epoch] = {'train_loss': train_loss, 'val_loss': val_loss, \
-                    "train_score": train_score, "val_score": val_score}
+                                           "train_score": train_score, "val_score": val_score}
             sava_data(self.result_save_path, learning_record_dict)
             print("\n")
